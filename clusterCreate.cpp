100c02f54f9c62b0456d614973df74bad986f03a
aalekh <aalekh1993@rediffmail.com>
2015-08-04 16:53:51 +0530

    Added Stop and Create function , renamed previous create function to start

--- clusterCreate.cpp
+++ clusterCreate.cpp
@@ -11,31 +11,12 @@ struct  clusterSpec {
     int timeout; //Idle Cluster Creation Timeout
     int nodes; // Number of Nodes in a Cluster
     int replicas; // No. of Slaves for each Master
+    int address;
 };
 
-int main(){
-
-cout << "Hello" <<endl;
-return 0;
-}
-
-int createCluster(clusterSpec c1) {
-    int EndPort = c1.port + c1.nodes;
-    stringstream ss;
-    char *command;
-    std::string test;
-    int port = c1.port;
-    while (port < EndPort){
-      cout << "Starting Ports" << endl;
-      port = port + 1;
-      clusterString(c1, port)
-      // command = to_string(ss);
-      system(command);
-    }
-    return 0;
-}
-
 char *clusterString(clusterSpec c1, int port){
+  string test;
+  char *command;
   test = "../../src/redis-server --port $PORT --cluster-enabled yes --cluster-config-file nodes-";
   strcat(command, test.c_str());
   strcat(command, to_string(port).c_str());
@@ -55,4 +36,58 @@ char *clusterString(clusterSpec c1, int port){
   strcat(command, test.c_str());
 
   return command;
+}
+
+int main(){
+
+cout << "Hello" <<endl;
+return 0;
+}
+
+int startCluster(clusterSpec c1) {
+    int EndPort = c1.port + c1.nodes;
+    stringstream ss;
+    char *command;
+    // std::string test;
+    int port = c1.port;
+    while (port < EndPort){
+      cout << "Starting Ports" << endl;
+      port = port + 1;
+      command = clusterString(c1, port);
+      // command = to_string(ss);
+      system(command);
+    }
+    return 0;
+}
+
+int createCluster(clusterSpec c1) {
+  char *host = "";
+  const char *address = to_string(c1.address).c_str();
+  strcat(host, address);
+  strcat(host, ":");
+  char *socketAddress;
+  int EndPort = c1.port + c1.nodes;
+  int port = c1.port;
+  while(port < EndPort){
+    port = port + 1;
+    strcat(host, to_string(port).c_str());
+    strcat(host, " ");
+  }
+  system(host);
+  return 0;
+}
+
+int stopCluster(clusterSpec c1) {
+  int EndPort = c1.port + c1.nodes;
+  int port = c1.port;
+  char *run;
+  while (port < EndPort) {
+    port = port + 1;
+    cout << " Stopping " << to_string(port) << endl;
+    strcat(run, "../../src/redis-cli -p ");
+    strcat(run, to_string(port).c_str());
+    strcat(run, " shutdown nosave");
+    system(run);
+  }
+  return 0;
 }
